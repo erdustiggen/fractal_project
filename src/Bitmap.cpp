1e781dc677a49de92c7f3e9775b50fe8689afd4a
erdustiggen <erdustiggen@gmail.com>
2018-02-13 18:32:51 +0100

    Update Bitmap.cpp

--- src/Bitmap.cpp
+++ src/Bitmap.cpp
@@ -6,14 +6,33 @@
  */
 
 #include "Bitmap.h"
+#include "BitmapFileHeader.h"
+#include "BitmapInfoHeader.h"
+#include<iostream>
+using namespace FractalProject;
 
 namespace FractalProject {
 
-Bitmap::Bitmap() {
+Bitmap::Bitmap(int width, int height):m_width(width), m_height(height), m_pPixel(new uint8_t[width*height*3]{}) {
 	// TODO Auto-generated constructor stub
 
 }
+bool Bitmap::write(std::string filename) {
 
+	BitMapFileHeader fileHeader;
+	BitmapInfoHeader infoHeader;
+
+	fileHeader.fileSize = sizeof(BitMapFileHeader) + sizeof(BitmapInfoHeader) + m_width*m_height*3;
+	fileHeader.dataOffset = sizeof(BitMapFileHeader) + sizeof(BitmapInfoHeader);
+
+	infoHeader.width = m_width;
+	infoHeader.height = m_height;
+
+	return false;
+}
+void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) {
+
+}
 Bitmap::~Bitmap() {
 	// TODO Auto-generated destructor stub
 }
