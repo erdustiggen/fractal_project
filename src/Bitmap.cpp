bb44285a51b9e4518e106f205171df8359d90960
erdustiggen <erdustiggen@gmail.com>
2018-02-13 17:52:35 +0100

    Create Bitmap.cpp

---
+++ src/Bitmap.cpp
@@ -0,0 +1,21 @@
+/*
+ * Bitmap.cpp
+ *
+ *  Created on: 13. feb. 2018
+ *      Author: ebras
+ */
+
+#include "Bitmap.h"
+
+namespace FractalProject {
+
+Bitmap::Bitmap() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Bitmap::~Bitmap() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace FractalProject */
