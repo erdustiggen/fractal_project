2bab8710ebcf3bfc18947f53fb02d11ec6dc68ce
erdustiggen <erdustiggen@gmail.com>
2018-02-13 18:34:16 +0100

    Update Bitmap.h

--- src/Bitmap.h
+++ src/Bitmap.h
@@ -7,15 +7,27 @@
 
 #ifndef BITMAP_H_
 #define BITMAP_H_
+#include<string.h>
+#include <cstdint>
+#include <memory> // In order to make unique_ptr work
+#include <iostream>
+
+
 
 namespace FractalProject {
 
+// Allocating memory to hold information about colors on screen
+
+
 class Bitmap {
 private:
-	int m_width;
-	int m_height;
+	int m_width{0};
+	int m_height{0};
+	std::unique_ptr<uint8_t[]> m_pPixel{nullptr};
 public:
 	Bitmap(int width, int height);
+	bool write(std::string filename);
+	void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
 	virtual ~Bitmap();
 };
 
